065d3020ec88164ee7c556a1f3d77528214bb95c
Tristan Burnham <tjburn70@gmail.com>
2021-05-25 19:52:50 -0500

    Add SEARCH_SERVICE_API to config

--- client/src/modules/config.js
+++ client/src/modules/config.js
@@ -1,6 +1,7 @@
 const getConfig = () => {
     const config = {
         footwedgeApiBaseUrl: process.env.REACT_APP_FOOTWEDGE_API_URL,
+        searchServiceApiUrl: process.env.REACT_APP_SEARCH_SERVICE_API_URL,
         environment: process.env.REACT_APP_ENV,
         auth: {
             region: process.env.REACT_APP_COGNITO_REGION,
